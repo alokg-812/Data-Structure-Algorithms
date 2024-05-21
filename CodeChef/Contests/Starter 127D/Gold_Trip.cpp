3763d42ce5a4e242007f4069d224812c98b80bc9
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2024-05-22 00:01:20 +0530

    solved using basic calculations

---
+++ CodeChef/Contests/Starter 127D/Gold_Trip.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T; //number of test cases
+    cout<<"Enter T: ";
+    cin>>T;
+    cout<<"Input number of test cases: "<<T<<endl;;
+	for(int i = 0 ; i < T ; i++){
+	    int N;
+        cout<<"Enter N: ";
+        cin>>N; // N denotes the total number of Islands.
+	    int arr[N];
+	    for(int j = 0 ; j < N ; j++){
+	        cin>>arr[j];	        
+	    }
+        cout<<"Input Array: ";
+        for(int k = 0 ; k < N ; k++){
+            cout<<arr[k]<<" ";
+        }
+        cout<<endl;
+        int Q;
+        cout<<"Enter Q value: ";
+        cin>>Q;
+        int Q1,Q2;
+        for(int j = 0 ; j < Q ; j++){
+            cin>>Q1>>Q2;  
+            int sum = 0 ;     
+            for(int z = Q1-1 ; z <= Q2-1 ; z++){
+                sum += arr[z];
+            }     
+            cout<<sum<<endl;
+        }
+	}
+}
