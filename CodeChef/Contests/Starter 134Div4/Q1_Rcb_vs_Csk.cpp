291a7ade746c06b3ddc6c2602d108019f2e16429
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2024-05-23 09:20:51 +0530

    Solved using basic conditionals

---
+++ CodeChef/Contests/Starter 134Div4/Q1_Rcb_vs_Csk.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	int x,y; // test cases
+    cin>>x>>y;
+
+    if((x-y)>=18){
+        cout<<"RCB\n";
+    }
+    else cout<<"CSK\n";
+}
