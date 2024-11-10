707f13759b3708647592516c5886cb4f10e266b2
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2024-11-10 23:14:53 +0530

    code

---
+++ CodeForces/Practice_Qs/734_A_Anton_and_Danik.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    string A[n];
+    int count_A = 0;
+    int count_D = 0;
+    for(int i = 0 ; i < n ; i++){
+        if(A[i] == 'A'){
+            count_A++;
+        }
+        if(A[i] = 'D'){
+            count_D++;
+        }
+    }
+}
