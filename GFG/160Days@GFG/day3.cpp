a38f8fabb8e228d9f6a28fc49826ec2e74ba551d
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2024-11-28 19:11:36 +0530

    Create day3.cpp

---
+++ GFG/160Days@GFG/day3.cpp
@@ -0,0 +1,47 @@
+https://www.geeksforgeeks.org/batch/gfg-160-problems/track/arrays-gfg-160/problem/reverse-an-array
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    void reverseArray(vector<int> &arr) {
+        // code here
+        int n = arr.size();
+        vector<int>temp;
+        for(int i=n-1;i>=0;i--){
+            temp.push_back(arr[i]);
+        }
+        arr = temp;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        ob.reverseArray(arr);
+        for (int i = 0; i < arr.size(); i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
