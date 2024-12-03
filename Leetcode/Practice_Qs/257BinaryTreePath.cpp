d4c94dc0eb66dc8b06e03f5b89666974921d106b
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2024-12-03 13:01:57 +0530

    Create 257BinaryTreePath.cpp

---
+++ Leetcode/Practice_Qs/257BinaryTreePath.cpp
@@ -0,0 +1,22 @@
+https://leetcode.com/problems/binary-tree-paths/submissions/1469037255/
+
+class Solution {
+public:
+    void helper(TreeNode* root, string s, vector<string>& ans){
+        if(root == NULL) return;
+        string a = to_string(root->val);
+        if(root->left == NULL and root->right == NULL){
+            s +=a;
+            ans.push_back(s);
+            return;
+        }
+        helper(root->left, s+a+"->", ans);
+        helper(root->right, s+a+"->", ans);
+    }
+
+    vector<string> binaryTreePaths(TreeNode* root) {
+        vector<string>ans;
+        helper(root, "", ans);
+        return ans;
+    }
+};
