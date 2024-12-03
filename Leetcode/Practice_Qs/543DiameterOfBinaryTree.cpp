721fa719b7655ccdf790f5443f648a911aa7df1b
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2024-12-03 09:49:17 +0530

    Update 543DiameterOfBinaryTree.cpp

--- Leetcode/Practice_Qs/543DiameterOfBinaryTree.cpp
+++ Leetcode/Practice_Qs/543DiameterOfBinaryTree.cpp
@@ -14,3 +14,27 @@ public:
         return maxDia;
     }
 };
+
+
+**********************************************************************************
+without using any global variable
+class Solution {
+public:
+    void helper(TreeNode* root, int &maxDia){
+        if(root == NULL) return;
+        int dia = levels(root->left) + levels(root->right);
+        maxDia = max(dia, maxDia);
+        helper(root->left,maxDia);
+        helper(root->right,maxDia);
+    }
+
+    int levels(TreeNode* root){
+        if(root==NULL) return 0;
+        return 1+ max(levels(root->left), levels(root->right));
+    }
+    int diameterOfBinaryTree(TreeNode* root) {
+        int maxDia = 0;
+        helper(root, maxDia);
+        return maxDia;
+    }
+};
