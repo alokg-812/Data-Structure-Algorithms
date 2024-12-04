d38c25b9cfdf888fccd3a21bdaad93b83c0d082b
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2024-12-04 20:45:23 +0530

    Create 1ChefSocks.cpp

---
+++ CodeChef/Contests/Starter163/1ChefSocks.cpp
@@ -0,0 +1,13 @@
+https://www.codechef.com/START163D/problems/CHEFSOCKS
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a,x,y;
+	cin>>a>>x>>y;
+	if(x+y>=a) cout<<"YES\n";
+	else cout<<"NO\n";
+
+}
