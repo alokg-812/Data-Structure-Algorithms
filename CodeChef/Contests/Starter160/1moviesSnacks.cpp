539a1e7a45dabf5b3b3b1e41cd7e8f624a8f0c34
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2024-12-06 23:35:47 +0530

    Create 1moviesSnacks.cpp

---
+++ CodeChef/Contests/Starter160/1moviesSnacks.cpp
@@ -0,0 +1,12 @@
+https://www.codechef.com/viewsolution/1113053815
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x,y,z;
+	cin>>x>>y>>z;
+	cout<<min((2*x + 3*y), (2*z)+y);
+
+}
