4a38791a576af47dbea79634a09b5f060e2c561d
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2024-12-12 11:23:12 +0530

    basic if else

---
+++ CodeChef/Contests/Starter164/1SPC2025.cpp
@@ -0,0 +1,13 @@
+https://www.codechef.com/problems/SPC2025
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int m,n;
+	cin>>n>>m;
+	if(m>=(n+1 )/2) cout<<"NEWBIE\n";
+	else cout<<"PRO\n";
+
+}
