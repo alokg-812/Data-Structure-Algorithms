581f27e4a0712ee7d75ee340bf52427fcc708fd5
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2024-12-19 08:24:23 +0530

    Create 1christmasCake.cpp

---
+++ CodeChef/Contests/Starter165/1christmasCake.cpp
@@ -0,0 +1,13 @@
+https://www.codechef.com/START165D/problems/CRCK
+https://www.codechef.com/viewsolution/1115880520
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	cout<<25-n;
+
+}
