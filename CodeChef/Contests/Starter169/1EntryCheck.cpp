7ddc1167276b1bc6b96afdc5d0b03aa20df42a8a
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2025-01-15 23:07:47 +0530

    code

--- CodeChef/Contests/Starter169/1EntryCheck.cpp
+++ CodeChef/Contests/Starter169/1EntryCheck.cpp
@@ -1,1 +1,12 @@
 https://www.codechef.com/problems/P1169
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x;
+	cin>>x;
+	if(x>=10) cout<<"YES\n";
+	else cout<<"NO\n";
+
+}
