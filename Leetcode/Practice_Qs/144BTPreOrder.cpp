c05b6368ee14cf3b69a3ad5acdfcefb2aaaaffa4
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2025-01-20 19:39:21 +0530

    code

---
+++ Leetcode/Practice_Qs/144BTPreOrder.cpp
@@ -0,0 +1,28 @@
+https://leetcode.com/problems/binary-tree-preorder-traversal/description/
+https://leetcode.com/problems/binary-tree-preorder-traversal/submissions/1514634841/
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> pre;
+    vector<int> preorderTraversal(TreeNode* root){
+        if(root == NULL) return pre;
+        else{
+            pre.push_back(root->val);
+            preorderTraversal(root->left);
+            preorderTraversal(root->right);
+        }     
+        return pre;
+    }
+};
