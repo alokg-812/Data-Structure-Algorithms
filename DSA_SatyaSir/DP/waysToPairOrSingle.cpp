a4a795451da0fe95f4daa9885ca391a49ed5e1d7
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2025-02-04 13:23:27 +0530

    dp

--- DSA_SatyaSir/DP/waysToPairOrSingle.cpp
+++ DSA_SatyaSir/DP/waysToPairOrSingle.cpp
@@ -1,6 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int ways(int n, vector<int>& arr){
+    if(n<=2){
+        arr[n] = n;
+        return arr[n];
+    }
+    if(arr[n]== -1){
+        arr[n] = ways(n-1, arr)+(n-1)* ways(n-2, arr);
+    }
+    return arr[n];
+}
 int main(){
     int n;
     cout<<"Enter n: ";
