e594734330c588e4ce0be73598114f70b0488635
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2025-02-04 13:21:28 +0530

    dp and backtracking

--- DSA_SatyaSir/DP/maxSubsequenceSum.cpp
+++ DSA_SatyaSir/DP/maxSubsequenceSum.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int maxSubSeq(int n, vector<int> arr, vector<int>& dp){
     if(n<0) return 0;
     if(dp[n]== -1){
-        dp[n] = max( arr[n] + maxSubSeq(n-1, arr, dp),maxSubSeq(n-2, arr, dp));
+        dp[n] = max(maxSubSeq(n-1, arr, dp),arr[n] + maxSubSeq(n-2, arr, dp));
     }
     return dp[n];
 }
