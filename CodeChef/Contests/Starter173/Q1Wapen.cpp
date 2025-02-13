62270f3c1c0f8377e94fdc8bff669035fe12fa76
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2025-02-13 22:04:07 +0530

    Create Q1Wapen.cpp

---
+++ CodeChef/Contests/Starter173/Q1Wapen.cpp
@@ -0,0 +1,12 @@
+https://www.codechef.com/START173C/problems/WAPEN
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x,y;
+	cin>>x>>y;
+	cout<<(x+(y*10))<<endl;
+
+}
