b22a5ae9be0b5ee6db74702dd514e665da752ef3
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2025-02-19 23:49:55 +0530

    Create Q1HomeWork.cpp

---
+++ CodeChef/Contests/Starter174/Q1HomeWork.cpp
@@ -0,0 +1,13 @@
+https://www.codechef.com/START174C/problems/HWFIN
+https://www.codechef.com/viewsolution/1134355551
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int x,y;
+    cin>>x>>y;
+    if((100-x)<=10*y) cout<<"Yes\n";
+    else cout<<"No\n";
+}
