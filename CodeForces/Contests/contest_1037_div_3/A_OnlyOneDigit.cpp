b36d606d71072e78b11455baaab422b5d9ca5389
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2025-07-17 22:57:21 +0530

    Create A_OnlyOneDigit.cpp

---
+++ CodeForces/Contests/contest_1037_div_3/A_OnlyOneDigit.cpp
@@ -0,0 +1,21 @@
+[question](https://codeforces.com/contest/2126/problem/A)
+
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int>arr;
+        while(n>0){
+            int d = n%10;
+            arr.push_back(d);
+            n=n/10;
+        }
+        sort(arr.begin(), arr.end());
+        cout<<arr[0]<<endl;
+    }
+}
