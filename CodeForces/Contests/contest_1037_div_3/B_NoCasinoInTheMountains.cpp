0fa0f80be55f7d01ddbad53a5114b42feec5942c
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2025-07-17 22:58:33 +0530

    Create B_NoCasinoInTheMountains.cpp

---
+++ CodeForces/Contests/contest_1037_div_3/B_NoCasinoInTheMountains.cpp
@@ -0,0 +1,33 @@
+[question]()
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n, k;
+        cin>>n>>k;
+        vector<int>arr(n);
+        for(int i = 0;i<n;i++){
+            cin>>arr[i];
+        }
+        int cnt =0,i=0;
+        while(i+k<=n){
+            bool flag = true;
+            for(int j=i;j<i+k;j++){
+                if(arr[j] ==1){
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag == true){
+                cnt++;
+                i=i+k+1;
+            }
+            else i++;
+        }
+        cout<<cnt<<endl;
+    }
+}
