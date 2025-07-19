8daa18b81bf7b7277c403bef75fc06a51a13cdf7
alokg-812 <alokji812@gmail.com>
2025-07-19 10:58:12 +0530

    learnt how to take multiple string inputs

---
+++ StriversSheet/C++ OneShot/basics.cpp
@@ -0,0 +1,73 @@
+#include <iostream>    // For input/output
+#include <string>      // For using strings
+using namespace std;
+
+// Function Example (outside main)
+void greetUser(string name) {
+    cout << "Hello, " << name << "! Welcome to learning C++!" << endl;
+}
+
+int addNumbers(int x, int y) {
+    return x + y;  // Returns sum of two numbers
+}
+
+int main() {
+
+    // 1. Output Statement
+    cout << "Welcome to Basic C++ Program!" << endl;
+
+    // 2. Variables and Data Types
+    int age = 20;
+    float height = 5.9;
+    char grade = 'A';
+    bool isStudent = true;
+    string name = "Alok";
+
+    // Displaying variable values
+    cout << "Name: " << name << ", Age: " << age << ", Height: " << height << endl;
+
+    // 3. Input from user
+    cout << "Enter your name: ";
+    cin >> name;   // Takes one word input
+    greetUser(name);  // Calling function
+
+    // 4. If-Else Condition
+    if (age >= 18) {
+        cout << "You are an adult." << endl;
+    } else {
+        cout << "You are a minor." << endl;
+    }
+
+    // 5. For Loop Example
+    cout << "Counting from 1 to 5: ";
+    for (int i = 1; i <= 5; i++) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    // 6. Array Example
+    int numbers[3] = {10, 20, 30};
+    cout << "Array elements: ";
+    for (int i = 0; i < 3; i++) {
+        cout << numbers[i] << " ";
+    }
+    cout << endl;
+
+    // 7. While Loop Example
+    int count = 3;
+    while (count > 0) {
+        cout << "Countdown: " << count << endl;
+        count--;
+    }
+
+    // 8. Function to add numbers
+    int sum = addNumbers(4, 5);
+    cout << "Sum of 4 and 5 using function: " << sum << endl;
+
+    // 9. Pointer Example
+    int num = 50;
+    int* ptr = &num;  // Pointer storing address of num
+    cout << "Value of num using pointer: " << *ptr << endl;  // Dereferencing pointer
+
+    return 0;
+}
