2c1e74470ea3f26071dd393fbcc90b9afb702635
alokg-812 <alokji812@gmail.com>
2025-07-20 11:25:42 +0530

    age using if else

---
+++ StriversSheet/C++ OneShot/ageValidation.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int age;
+    cin>>age;
+    if(age>=18){
+        cout<<"You are an adult!\n";
+    }
+    else cout<<"You are Under 18!\n";
+}
