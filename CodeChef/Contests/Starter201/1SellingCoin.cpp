b9433b4113d6c231202c92b9f731b3665a2afdd9
alokg-812 <alokji812@gmail.com>
2025-08-27 21:55:47 +0530

    contest codes

---
+++ CodeChef/Contests/Starter201/1SellingCoin.cpp
@@ -0,0 +1,13 @@
+// https://www.codechef.com/START201D/problems/SELLCOIN
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a,b;
+	cin>>a>>b;
+	cout<<(a+(2*b))<<endl;
+
+}
