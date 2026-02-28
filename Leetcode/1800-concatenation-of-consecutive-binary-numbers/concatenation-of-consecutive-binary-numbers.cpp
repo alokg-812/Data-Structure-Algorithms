965d18079a2259937a82a2f9ef32aee4c98e8cb6
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2026-02-28 19:15:39 +0530

    Time: 24 ms (82.55%) | Memory: 8 MB (89.26%) - LeetSync

---
+++ Leetcode/1800-concatenation-of-consecutive-binary-numbers/concatenation-of-consecutive-binary-numbers.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int concatenatedBinary(int n) {
+        long long ans = 0;
+        long long MOD = 1e9 + 7;
+        int length = 0;
+        
+        for(int i = 1;i<=n;i++){
+            if((i & (i-1)) == 0){
+                length++;
+            }
+            ans=((ans<<length) | i) % MOD;
+        }
+        return ans;
+    }
+};
