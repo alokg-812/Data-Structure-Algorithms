9a97dac63b005eef253939634507c78d9b5964e9
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2026-04-02 15:19:54 +0530

    Time: 0 ms (100.00%) | Memory: 27.6 MB (72.78%) - LeetSync

---
+++ Leetcode/3952-trionic-array-i/trionic-array-i.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isTrionic(vector<int>& nums) {
+        int n = nums.size();
+        if(nums[0]>=nums[1]) return false;
+        int cnt=0;
+        for(int i=2;i<n;i++){
+            if(nums[i-1] == nums[i]){
+                return false;
+            }
+            if((nums[i-2]-nums[i-1])* (nums[i-1]-nums[i]) < 0){
+                cnt++;
+            }
+        }
+        return cnt==2;
+        
+    }
+};
