54d5d2c87bffe73ad2690ecb49b9ecc357542fdd
Alok Gupta <154364202+alokg-812@users.noreply.github.com>
2026-04-18 22:14:06 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (30.08%) - LeetSync

---
+++ Leetcode/4168-mirror-distance-of-an-integer/mirror-distance-of-an-integer.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int reverseNum(int x){
+        int rev = 0;
+        while(x!=0){
+            rev = rev*10 + x%10;
+            x /=10;
+        }
+        return rev;
+    }
+    int mirrorDistance(int n) {
+        int rev = reverseNum(n);
+        return abs(rev-n);    
+    }
+};
